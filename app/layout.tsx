5a866017b969c071a0a0e3126f41a5685210e204
Dharmesh Ranpariya <dharmeshranpariya@gmail.com>
2026-01-17 09:51:28 +0530

    add script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -28,12 +29,33 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
         <DisableInspect />
         <Header />
+
         <main className="flex-grow">{children}</main>
+
         <Footer />
+
+        {/* Ad configuration */}
+        <Script id="ad-options" strategy="afterInteractive">
+          {`
+            var atOptions = {
+              key: '64dd8e3b1eb7f1402f3c31705ca08ab9',
+              format: 'iframe',
+              height: 250,
+              width: 300,
+              params: {}
+            };
+          `}
+        </Script>
+
+        {/* Ad script */}
+        <Script
+          src="https://www.highperformanceformat.com/64dd8e3b1eb7f1402f3c31705ca08ab9/invoke.js"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
